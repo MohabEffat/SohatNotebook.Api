62c542a013a8e024d4ccef64457cdcebdbe31100
Mohab <Mohabeffat76@gmail.com>
2024-11-23 14:43:47 +0200

    Add project files.

---
+++ Entites/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities.Dtos
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ SohatNotebook.Api/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using DataService.Data;
+using Entities.DbSet;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SohatNotebook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.Where(x => x.Status == 1).ToList();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(UserDto userDto)
+        {
+            var user = new User
+            {
+                FirstName = userDto.FirstName,
+                LastName = userDto.LastName,
+                Email = userDto.Email,
+                DateOfBirth = userDto.DateOfBirth,
+                Country = userDto.Country,
+                Phone = userDto.Phone,
+                Status = 1
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("GetUser")]
+        //[Route("GetUser")]
+        public IActionResult GetUser(Guid Id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == Id);
+
+            return Ok(user);
+        }
+    }
+}
